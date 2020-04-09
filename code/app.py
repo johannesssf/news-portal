f8c88208fbd7ffc1c1cc17ba0d9f82c1c195b2c3
Johannes Ferreira <johannesssf@gmail.com>
2020-04-09 14:13:33 -0300

    Flask news app

---
+++ code/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_restful import Api
+
+from resources.author import AuthorResource, AuthorList
+from resources.news import NewsResource, NewsList
+
+
+app = Flask(__name__)
+app.secret_key = 'EV1}YJf,>i~S%1v:],jYijl^%4mG1q'
+api = Api(app)
+
+api.add_resource(AuthorList, '/authors')
+api.add_resource(AuthorResource, '/author/<string:name>')
+
+api.add_resource(NewsList, '/news/search')
+api.add_resource(NewsResource, '/news',
+                               '/news/<int:news_id>')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
